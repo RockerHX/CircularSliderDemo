f38b8f5515144a9cfddebbb3eba179b3bf2cbed5
RockerHX <RockerHX@gmail.com>
2016-04-29 17:59:51 +0800

    Initial Commit

---
+++ CircularSliderDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CircularSliderDemo
+//
+//  Created by miaios on 16/4/29.
+//  Copyright © 2016年 Mia Music. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CircularSliderDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CircularSliderDemo
+//
+//  Created by miaios on 16/4/29.
+//  Copyright © 2016年 Mia Music. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
